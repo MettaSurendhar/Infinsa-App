d1c21d1a6ce3db165ee38a89393a8e003f19e45e
Metta Surendhar <msurendhar8815@gmail.com>
2025-01-10 21:30:46 +0530

    Update supabase.dart

--- lib/backend/supabase/supabase.dart
+++ lib/backend/supabase/supabase.dart
@@ -2,9 +2,9 @@ import 'package:supabase_flutter/supabase_flutter.dart' hide Provider;
 
 export 'database/database.dart';
 
-String _kSupabaseUrl = 'https://pugdfiszyxjpuclgtxfj.supabase.co';
+String _kSupabaseUrl = '';
 String _kSupabaseAnonKey =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1Z2RmaXN6eXhqcHVjbGd0eGZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI0MDYxMDAsImV4cCI6MjA0Nzk4MjEwMH0.fgduJLdXSIz-nV-KCrgU7YKtlo1UDjaaNGfipeMAt7o';
+    '';
 
 class SupaFlow {
   SupaFlow._();
